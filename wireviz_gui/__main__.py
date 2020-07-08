3c06f266a5a5e9e5e5bc402a24267f86341fdb25
Jason R. Jones <slightlynybbled@gmail.com>
2020-07-08 13:02:11 -0400

    Add ability to load graphviz path directly

--- wireviz_gui/__main__.py
+++ wireviz_gui/__main__.py
@@ -1,5 +1,16 @@
 import logging
+import os
+from pathlib import Path
+
 from wireviz_gui.app import Application
+
+
+_graphviz_path = Path(__file__).parent / '..' / 'graphviz_238_win32' / 'bin'
+
+if os.environ['PATH'].endswith(os.pathsep):
+    os.environ["PATH"] += str(_graphviz_path)
+else:
+    os.environ["PATH"] += os.pathsep + str(_graphviz_path)
 
 logging.basicConfig(level=logging.DEBUG)
 Application()
