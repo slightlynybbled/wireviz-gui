65604e6c7c09b22b70a6b522312b723b0b7d1bd4
Jason R. Jones <slightlynybbled@gmail.com>
2020-07-08 14:52:42 -0400

    Adding click library to allow easy command-line switches.

--- wireviz_gui/__main__.py
+++ wireviz_gui/__main__.py
@@ -2,15 +2,24 @@ import logging
 import os
 from pathlib import Path
 
+import click
+
 from wireviz_gui.app import Application
 
 
-_graphviz_path = Path(__file__).parent / '..' / 'graphviz_238_win32' / 'bin'
+@click.command()
+@click.option('--graphviz-path', '-p', default=None, help='The path on which GraphViz binary may be found.')
+def main(graphviz_path):
+    if graphviz_path is not None:
+        _graphviz_path = Path(__file__).parent / '..' / 'graphviz_238_win32' / 'bin'
 
-if os.environ['PATH'].endswith(os.pathsep):
-    os.environ["PATH"] += str(_graphviz_path)
-else:
-    os.environ["PATH"] += os.pathsep + str(_graphviz_path)
+        if os.environ['PATH'].endswith(os.pathsep):
+            os.environ["PATH"] += str(_graphviz_path)
+        else:
+            os.environ["PATH"] += os.pathsep + str(_graphviz_path)
 
-logging.basicConfig(level=logging.DEBUG)
-Application()
+    logging.basicConfig(level=logging.DEBUG)
+    Application()
+
+
+main()
